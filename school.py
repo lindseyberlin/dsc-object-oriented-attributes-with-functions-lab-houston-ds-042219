db52b347da7fb1fae0f29ececf033413a78a737b
Lindsey Berlin <lindsey.berlin@flatironschool.com>
2019-09-04 15:49:37 -0500

    Added local changes

--- school.py
+++ school.py
@@ -1,1 +1,22 @@
-class School:
+class School():
+    def __init__(self, name=None, roster={}):
+        self.name = name
+        self.roster = {}
+
+    def add_student(self, full_name, grade_level):
+        if grade_level in self.roster:
+            self.roster[grade_level].append(full_name)
+        else:
+            self.roster[grade_level] = [full_name]
+
+        # Could also be written as:
+        # self.roster[grade_level] = self.roster.get(grade_level, []).append(full_name)
+        
+    def grade(self, grade_level):
+        return self.roster[grade_level]
+    
+    def sort_roster(self):
+        sorted_dict = self.roster
+        for key in sorted_dict:
+            sorted_dict[key].sort()
+        return sorted_dict
